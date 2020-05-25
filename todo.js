d46bf3207176091e8686411227bb6044cc4f542b
ruchikh <ruchikharwar4@gmail.com>
2020-05-25 09:50:18 +0530

    completed some todo functionality

--- todo.js
+++ todo.js
@@ -1,72 +1,72 @@
-console.log('start todo');
 var form = document.querySelector('.form');
 var input = document.querySelector('.todo-input');
 var ul = document.querySelector('.todo-list');
+
+var todos = [];   
 var apiUrl = 'https://jsonplaceholder.typicode.com/todos/'
 
 function addTodo(event) {
-    event.preventDefault()
+    event.preventDefault();
+    if(input.value === '') return
+
+    var todoObj = {
+        title: input.value,
+        completed: false,
+    }
+    
     fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            title: input.value,
-            completed: false,
-        })
-    })
-        .then(resp => resp.json())
-        .then(todoLists)
+        body: JSON.stringify(todoObj)
+        })                            // the status for this request is 200 but the database is not getting update.
+    todos = [...todos, todoObj];
+    displayTodoList(todos);
     input.value = '';
-}
-
-function todoLists(todo) {
-    console.log(todo, 'todo');
-    ul.innerHTML += `<li>
-            <input type="checkbox"/>
-            <span>${todo.title}</span>
-            <button class='btn' id=${todo.id}>X</button>
-        </li>
-        `;
-    // var l = document.createElement("li")
-    // var span = document.createElement("span")
-    // // span.setAttribute("contenteditable", true)
-    // // span.setAttribute("data-id", todo.id)
-    // span.innerHTML = todo.title;
-    // // span.addEventListener("blur", update)
-    // l.appendChild(span)
-    // var input = document.createElement("input")
-    // var a = document.createElement("a")
-    // a.innerHTML = '&times;'
-    // a.addEventListener("click", deleteTodo)
-    // a.setAttribute("data-id", todo.id)
-    // input.setAttribute("type", 'checkbox')
-    // l.prepend(input)
-    // l.appendChild(a)
-    // ul.appendChild(l)
 }
 
 
 function deleteTodo(e) {
     e.preventDefault()
+    if(e.target.className !== "todo-delete-btn") return;
+
     var id = e.target.id;
-    let delButtonIsPressed = event.target.className === "btn"
-    console.log(delButtonIsPressed, id, 'delButtonIsPressed')
-    console.log(apiUrl + id)
-    if (delButtonIsPressed) {
-        fetch(apiUrl + id, {
-            method: 'DELETE'
-        }).then(resp => resp.json()).then(getTodoList)
-    }
+
+    fetch(apiUrl + id, {
+        method: 'DELETE'
+    })                                   // Similarly it returns a empty obj {} in response. 
+    todos.splice(id, 1);
+    displayTodoList(todos);
+}
+
+function displayTodoList(arr= []) {
+    ul.innerHTML = '';
+    arr.forEach((arrItem, i) => {
+        ul.innerHTML += 
+        `<li class='todo-box'>
+            <input type="checkbox" class="todo-checkbox" id=${i} ${arrItem.completed ? 'checked' : ""} />
+            <label class="todo-item">${arrItem.title}</label>
+            <button class='todo-delete-btn' id=${i}>X</button>
+        </li>
+        `;
+    })
 }
 
 function getTodoList() {
-    fetch(apiUrl + '?_limit=10').then(res => res.json()).then(lists =>
-        lists.forEach(todoLists)
-    )
+    fetch(apiUrl + '?_limit=10').then(res => res.json()).then(data => {
+        todos = data;
+        displayTodoList(todos)
+    })
 }
 
+function markAsDone(e) {
+    if(e.target.className !== 'todo-checkbox') return;
+    var id = e.target.id;
+    todos[id].completed = !todos[id].completed;
+}
 
-ul.addEventListener('click', deleteTodo)
-form.addEventListener('submit', addTodo)
+ul.addEventListener('click', deleteTodo);
+ul.addEventListener('click', markAsDone);
+form.addEventListener('submit', addTodo);
+window.addEventListener('DOMContentLoaded', getTodoList)
